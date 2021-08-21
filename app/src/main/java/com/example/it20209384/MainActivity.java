352ec429db97aedc0bcace02f98a9cbdc7ccbc92
IsuruManjitha <IT20209384@my.sliit.lk>
2021-08-21 16:55:06 +0530

    Additional tutorial

---
+++ app/src/main/java/com/example/it20209384/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.it20209384;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.SparseIntArray;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+     private static final int CAMERA_REQUEST=1888;
+     ImageView imageView;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = (ImageView)this.findViewById(R.id.imageView);
+        Button photoButton = (Button) this.findViewById(R.id.button);
+
+        photoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraintent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraintent, CAMERA_REQUEST);
+
+
+            }
+        });
+    }
+}
